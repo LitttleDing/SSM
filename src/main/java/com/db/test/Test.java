8c48bf5ac726ecfcab126bad7fb845bb47a41582
dingbo <845064749@qq.com>
2017-10-14 22:40:41 +0800

    初始化

---
+++ src/main/java/com/db/test/Test.java
@@ -0,0 +1,24 @@
+package com.db.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping( "/test" )
+public class Test {
+    @RequestMapping ( "/data" )
+    @ResponseBody
+    public String data(HttpServletRequest req)
+    {
+        return "fuck this !";
+    }
+
+    @RequestMapping ( "/page" )
+    public String page(HttpServletRequest req)
+    {
+        return "test";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>you are welcome budy !</h2>
+</body>
+</html>
